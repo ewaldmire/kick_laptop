1446628d13cf6bee5c8d265f44d712bbadaa7a28
Eddie Waldmire <eddie.waldmire@gmail.com>
2021-07-16 10:09:43 -0500

    Add Rocky 8

--- post_scripts/hammer_create_os.sh
+++ post_scripts/hammer_create_os.sh
@@ -46,8 +46,6 @@ hammer os set-default-template \
 --id $OSID \
 --provisioning-template-id $PXELID #Kickstart default PXELinux
 
-
-
 ##################
 #CentOS 8        #
 ##################
@@ -61,6 +59,39 @@ hammer os create \
 
 #Find and set OS and Template IDs
 OSID=$(hammer --csv os list --search 'title = "CentOS 8"' | grep -v Id | awk -F, {'print $1'})
+FNSHID=$(hammer --csv template list --search 'name = "Kickstart default finish"' | grep -v Id | awk -F, {'print $1'})
+PROVID=$(hammer --csv template list --search 'name = "Kickstart default"' | grep -v Id | awk -F, {'print $1'})
+PXELID=$(hammer --csv template list --search 'name = "Kickstart default PXELinux"' | grep -v Id | awk -F, {'print $1'})
+
+#Update the Finish template
+hammer os set-default-template \
+--id $OSID \
+--provisioning-template-id $FNSHID #Kickstart default finish
+
+#Update the Provisioning template
+hammer os set-default-template \
+--id $OSID \
+--provisioning-template-id $PROVID #Kickstart default
+
+#Update the PXELinux template
+hammer os set-default-template \
+--id $OSID \
+--provisioning-template-id $PXELID #Kickstart default PXELinux
+
+
+##################
+#Rocky 8        #
+##################
+hammer os create \
+--name Rocky \
+--major 8 \
+--architectures x86_64 \
+--partition-tables "Kickstart default" \
+--media "RL8 Base" \
+--provisioning-templates "Kickstart default,Kickstart default finish,Kickstart default PXELinux"
+
+#Find and set OS and Template IDs
+OSID=$(hammer --csv os list --search 'title = "Rocky 8"' | grep -v Id | awk -F, {'print $1'})
 FNSHID=$(hammer --csv template list --search 'name = "Kickstart default finish"' | grep -v Id | awk -F, {'print $1'})
 PROVID=$(hammer --csv template list --search 'name = "Kickstart default"' | grep -v Id | awk -F, {'print $1'})
 PXELID=$(hammer --csv template list --search 'name = "Kickstart default PXELinux"' | grep -v Id | awk -F, {'print $1'})
